8c4fe770054da214b7a4cd66ac0bfd3b512722a0
muthu <anushyamuthu97@gmail.com>
2024-10-18 17:04:27 +0530

    sub

--- server/api/index.js
+++ server/api/index.js
@@ -7,7 +7,7 @@ const nodemailer = require("nodemailer");
 const app = express();
 const port = 8000;
 const cors = require("cors");
-const cors = require("cors");
+// const cors = require("cors");
 app.use(cors({ origin: "https://books-client-iaft.onrender.com" })); // Allow only your frontend
 
 app.use(express.json());
