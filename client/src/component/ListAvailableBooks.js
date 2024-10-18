5bd06557d84cdbd20d7f5c244cc656147ff00f52
muthu-anushya <126173217+muthu-anushya@users.noreply.github.com>
2024-10-18 13:11:50 +0530

    Update ListAvailableBooks.js

--- client/src/component/ListAvailableBooks.js
+++ client/src/component/ListAvailableBooks.js
@@ -19,7 +19,7 @@ function ListAvailableBooks() {
   // Fetch books from the backend
   useEffect(() => {
     axios
-      .get("http://localhost:8000/getbooks")
+      .get("https://books-api-nu.vercel.app/getbooks")
       .then((response) => {
         setBooks(response.data);
       })
